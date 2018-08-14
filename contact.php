56f5782da71fe6c289291d3447a9bc70e4dca763
Deepak Yadav <37175057+DE-co-DE@users.noreply.github.com>
2018-08-14 23:14:20 +0530

    few

---
+++ contact.php
@@ -0,0 +1,77 @@
+<?php
+if(isset($_POST['submit'])) {
+    $email_to = "deep7rd@gmail.com";
+    $email_subject = "From De-co-DE";
+ 
+    function died($error) {
+      
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+     
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+ 
+ 
+   
+    if(!isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['telephone']) ||
+        !isset($_POST['comments'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+ 
+     
+ 
+    $first_name = $_POST['name']; // required
+
+    $email_from = $_POST['email']; // required
+    $telephone = $_POST['telephone']; // not required
+    $comments = $_POST['comments']; // required
+ 
+    $error_message = "";
+   
+ 
+
+ 
+
+ 
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+ 
+    $email_message = "Message details below.\n\n";
+ 
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+ 
+     
+ 
+    $email_message .= "Name: ".clean_string($first_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+    $email_message .= "Message: ".clean_string($comments)."\n";
+ 
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+if(mail($email_to, $email_subject, $email_message, $headers)){
+	echo '<script>alert("Your message is sent");</script>';
+}
+?>
+ 
+<!-- include your own success html here -->
+ 
+Thank you for contacting us. We will be in touch with you very soon.
+ 
+<?php
+ 
+}
+header('location:https://de-co-de.github.io/contact.html?msg=send');
+
+?>
