39a86fc1b63a1afb0f585ee5d3080561eb4dde04
Deepak Yadav <37175057+DE-co-DE@users.noreply.github.com>
2018-09-02 23:14:59 +0530

    an assign

---
+++ bolehmedia/js/theme.js
@@ -0,0 +1,28 @@
+// -----------------------------
+
+//   js index
+/* =================== */
+/*  
+    
+
+    
+
+*/
+// -----------------------------
+
+
+(function($) {
+    "use strict";
+
+
+// headroom js activation
+
+var myElement = document.querySelector("header");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement);
+// initialise
+headroom.init(); 
+
+
+
+}(jQuery));
